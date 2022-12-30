e45e39cb5a8407565e6c78ed5fd603b97f77bdc2
MFabri31 <medinajose3195@gmail.com>
2022-12-30 19:14:19 -0300

    Get a user by id

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,7 +5,14 @@ const userModel = require("../models/user.model");
 router.get("/api/users", (req, res) => {
   userModel.find({}, (error, data) => {
     if (error) res.json({ status: 500, error });
-    return res.json({ status: 200, data });
+    res.json({ status: 200, data });
+  });
+});
+
+router.get("/api/users/:id", (req, res) => {
+  userModel.find({ id: req.params.id }, (error, data) => {
+    if (error) res.json({ status: 500, error });
+    res.json({ status: 200, data });
   });
 });
 
